ee82fd0f320275bf79d0fc99add07b6f69b3d04d
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-09-16 14:53:51 +0100

    Fixes and adjustments in logs

--- twitty/retweeter.py
+++ twitty/retweeter.py
@@ -12,27 +12,28 @@ def main():
     api = creates_twitter_object()
     #the usernames of the account you wanna get tweets
     screen_names = ["realpython", "geeksforgeeks","dsc_ubuea", "djangoproject", "ThePSF"]
-    logger.info("Fetching tweets")
+    logger.info("Fetching tweets all tweets")
     for screen_name in screen_names:
-    	print(screen_name)
+    	logger.info(f"Fetching tweets of {screen_name}")
+    	print("Fetching tweets of "+ screen_name)
     	#count = 2, looks only for the latest 2 tweets for each user
     	statuses = api.user_timeline(screen_name, count = 2)
     	for status in statuses:
-    		print(status.text)
-    		#api.retweet(status.id)
-    		#api.unretweet(status.id)
+    		if status.in_reply_to_status_id is not None:
+    			# if tweet is a reply continue
+    			continue
     		if not status.favorited:
     		          # Like the the tweet
     		          try:
-    		          	api.favorite(status.id)
+    		          	status.favorite()
     		          except Exception as e:
-    		          	logger.error("Error while liking tweet with id %(status.id)", exc_info=True)
+    		          	logger.error(f"Error while liking tweet with id {status.id}", exc_info=True)
     		if not status.retweeted:
     		      #Retweet, since we have not retweeted it yet
     		      try:
     		      	api.retweet(status.id)
     		      except Exception as e:
-    		      	logger.error("Error while retweeting tweet with id %(status.id)", exc_info=True)
+    		      	logger.error(f"Error while retweeting tweet with id {status.id}", exc_info=True)
 
 
 if __name__ == "__main__":
